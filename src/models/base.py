04f1271a99a88867c154ec12c5131ae546651f49
Shenoy (Instance) <shenoy@wadhwaniai.org>
2021-05-25 16:00:52 +0530

    Added Directory Structure

---
+++ src/models/base.py
@@ -0,0 +1,47 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+
+class Model(pl.LightningModule):
+    """Base class for any machine learning model using Pytorch Lightning
+
+    :param config: Config object
+    :type config: Config
+    """
+    def __init__(self, config):
+        super().__init__()
+        self.setup_configs(config)
+
+        self.setup_network()
+        
+    def _setup_configs(self, config):
+        self.network_config = config.network
+        self.model_config = config.model
+
+    def _setup_network(self):
+        # TODO : Insert Network 
+
+    def _setup_optimizers(self, model_config):
+        return None
+
+    def configure_optimizers(self):
+        return _setup_optimizers(self.model_config)        
+        
+    def training_step(self, batch, batch_idx):
+        return None
+
+    def training_step_end(self, batch_parts):
+        return None
+
+    def training_epoch_end(self, training_step_outputs):
+        return None 
+
+    def validation_step(self, batch, batch_idx):
+        return None
+
+    def validation_step_end(self, batch_parts):
+        return None
+
+    def validation_epoch_end(self, validation_step_outputs):
+        return None
